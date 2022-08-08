7e0634e8b5b056cf23755dd879c5d41c294774d2
Jp <jayaprakashnis619@gmail.com>
2022-08-08 22:50:26 +0530

    modified bst.py

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -4,20 +4,32 @@ class binarySearchTree():
         self.left = None
         self.val = key
 
-    def insertTree(self, root, key):
-        if root is None:
-            return binarySearchTree(key)
-        else:
-            if root.val == key:
-                return root
-            elif root.val<key:
-                root.right=self.insertTree(root.right,key)
-            else:
-                root.left=self.insertTree(root.left,key)
 
-    def inorder(self,root):
-        if root:
-            self.inorder(root.left)
-            print(root.val)
-            self.inorder(root.right)
-            
+def insertTree(root, key):
+    if root is None:
+        return binarySearchTree(key)
+    else:
+        if root.val == key:
+            return root
+        elif root.val < key:
+            root.right = insertTree(root.right, key)
+        else:
+            root.left = insertTree(root.left, key)
+    return root
+
+
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
+
+
+if __name__ == "__main__":
+    bst = binarySearchTree(40)
+    bst = insertTree(bst, 30)
+    bst = insertTree(bst, 20)
+    bst = insertTree(bst, 50)
+    bst = insertTree(bst, 60)
+
+    inorder(bst)
