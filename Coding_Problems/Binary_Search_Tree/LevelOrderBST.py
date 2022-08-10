dee078f75faf2c364d42e5719e49e1c46db98d76
Jp <jayaprakashnis619@gmail.com>
2022-08-10 21:57:54 +0530

    testing inorder.py

--- Coding_Problems/Binary_Search_Tree/LevelOrderBST.py
+++ Coding_Problems/Binary_Search_Tree/LevelOrderBST.py
@@ -1,26 +1,35 @@
 from collections import deque
-from re import L
 
 
 class Node():
-    def __init__(self, val=0):
-        self.left = None
-        self.right = None
+    def __init__(self, val=0, left=None, right=None):
+        self.left = left
+        self.right = right
         self.val = val
 
-    def levelOrder(self, root):
-        result = []
-        queue = deque()
-        queue.append(root)
 
-        while queue:
-            level = []
-            for i in range(len(level)):
-                node = queue.pop()
-                if node:
-                    level.append(node)
-                    queue.append(root.left)
-                    queue.append(root.right)
-            if level:
-                result.append(level)
-        return result
+def levelOrder(root):
+    result = []
+    queue = deque()
+    queue.append(root)
+
+    while queue:
+        level = []
+        for i in range(len(level)):
+            node = queue.pop()
+            if node:
+                level.append(node)
+                queue.append(node.left)
+                queue.append(node.right)
+        if level:
+            result.append(level)
+    return result
+
+
+root1 = Node(3)
+root1.left = Node(9)
+root1.right = Node(20)
+# root1.right.left = Node(15)
+# root1.right.right = Node(7)
+
+print(levelOrder(root1))
