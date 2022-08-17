0cddc21b83412fe1fdfe1c03e32fb33001b7451b
Jp <jayaprakashnis619@gmail.com>
2022-08-17 23:52:53 +0530

    Small modification in  range

--- Coding_Problems/Arrays,Strings,Linkedlists/isUnique.py
+++ Coding_Problems/Arrays,Strings,Linkedlists/isUnique.py
@@ -1,6 +1,6 @@
 def IsUnique(string):
-    for i in range(0, len(string)-1, 1):
-        for j in range(len(string)-1, 0, -1):
+    for i in range(0, len(string)-1):
+        for j in range(len(string)-1, 0,):
             if string[i] == string[j] and i != j:
                 return False
     return True
