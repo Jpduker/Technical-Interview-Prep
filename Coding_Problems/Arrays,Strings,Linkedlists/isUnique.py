4c24c40fa85305b0f2a5e959bd456263b5807428
Jp <jayaprakashnis619@gmail.com>
2022-08-17 23:51:58 +0530

    new  base condition added

--- Coding_Problems/Arrays,Strings,Linkedlists/isUnique.py
+++ Coding_Problems/Arrays,Strings,Linkedlists/isUnique.py
@@ -1,8 +1,9 @@
 def IsUnique(string):
     for i in range(0, len(string)-1, 1):
         for j in range(len(string)-1, 0, -1):
-            if string[i] == string[j]:
+            if string[i] == string[j] and i != j:
                 return False
+    return True
 
 
 print(IsUnique("aple"))
