fa0ada7cf2f66172ee2eb96ac152c9c820185e14
Jp <jayaprakashnis619@gmail.com>
2022-10-09 11:06:34 +0530

    bottom_up

--- climbingStairsDP.cpp
+++ climbingStairsDP.cpp
@@ -24,7 +24,25 @@ int climbingStairs(int n)
     return dp(n);
 }
 
+// Bottom Up Approach
+
+int Climbing_stairs(int n)
+{
+
+    // An array that represents the answer to the problem for a given state
+    int dp[10];
+    // Base Cases
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i < n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
+
 int main()
 {
-    cout << climbingStairs(5);
+    cout << Climbing_stairs(5);
 }
