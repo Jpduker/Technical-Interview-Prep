7d7b85dd88fde0ae857e477e7e0f743b167aa7eb
Jp <jayaprakashnis619@gmail.com>
2022-10-09 10:46:38 +0530

    dp_climbingStairs

---
+++ climbingStairsDP.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int dp(int i)
+{
+    unordered_map<int, int> hash;
+    // base cases
+    if (i <= 2)
+    {
+        return i;
+    }
+    if (hash.find(i) == hash.end())
+    {
+        hash.insert({i, dp(i - 1) + dp(i - 2)});
+    }
+    return hash.at(i);
+}
+
+int climbingStairs(int n)
+{
+    return dp(n);
+}
+
+int main()
+{
+    cout << climbingStairs(5);
+}
