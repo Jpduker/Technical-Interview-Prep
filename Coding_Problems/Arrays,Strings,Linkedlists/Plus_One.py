ba0e68e23cf1eceb8a53b947b655801b2d609043
Jpduker <jayaprakashnis619@gmail.com>
2022-11-02 12:02:43 +0530

    Added PlusOne code

---
+++ Coding_Problems/Arrays,Strings,Linkedlists/Plus_One.py
@@ -0,0 +1,5 @@
+def PlusOne(nums):
+    nums[len(nums)-1]+=1
+    return nums
+
+print(PlusOne([3,0,0,1]))
--- Coding_Problems/Arrays,Strings,Linkedlists/plusOne.cpp
+++ Coding_Problems/Arrays,Strings,Linkedlists/plusOne.cpp
@@ -1,14 +1,38 @@
+#include<iostream>
+#include <bits/stdc++.h>
+#include<vector>
+
+using namespace std;
+
+
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        vector<int> result;
-        int len = digits.size();
-        for(int i=0;i<len;i++){
-            result[i]=digits[i];
-        }
-        result[len]+=1;
-        
-        return result;
+         for(int i = digits.size()-1; i>=0; i--){
+                    if(digits[i]<9){
+                    digits[i]++;
+                    return digits;
+                    }
+                    else
+                            digits[i] = 0;
+                    
+            }
+            digits.push_back(0);
+            digits[0] = 1;
+            return digits;
         
     }
 };
+int main(){
+    Solution obj;
+    vector<int> nums;
+    nums.push_back(9);
+    nums.push_back(9);
+    nums.push_back(9);
+    obj.plusOne(nums);
+    for(int i=0;i<nums.size();i++){
+        cout<<nums[i];
+    }
+    return 0;
+}
+
