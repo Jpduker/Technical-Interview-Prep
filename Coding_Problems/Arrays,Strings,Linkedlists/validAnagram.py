f851a7f0591d6f2c69d3efccf0584869c2ef500e
Jp <jayaprakashnis619@gmail.com>
2023-01-17 16:16:55 +0530

    modified anagram.py

--- Coding_Problems/Arrays,Strings,Linkedlists/validAnagram.py
+++ Coding_Problems/Arrays,Strings,Linkedlists/validAnagram.py
@@ -1,23 +1,22 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        hashmap={}  
-        #Base condition
-        if len(s)!=len(t):
-            return False 
-        #add elements of one string into the hashmap completely
-        for i in range(0,len(s)):
-            hashmap[s[i]]=1+hashmap.get(s[i],0)   
-        
-        #Loop through the second hashmap and check if the element   present in 2nd string is in the hashmap
-        #if its not present simply return false else return true.
-        for j in range(0,len(t)):
-            if t[j] not in hashmap:
-                return False
-            if t[j] in hashmap and not hashmap[t[j]]>0:
-                return False
-            if t[j] in hashmap and hashmap[t[j]]>0:
-                hashmap[t[j]]=1-hashmap.get(t[j],0)
-        return True
-            
+def isAnagram(s, t):
+    hashmap={}  
+    #Base condition
+    if len(s)!=len(t):
+        return False 
+    #add elements of one string into the hashmap completely
+    for i in range(0,len(s)):
+        hashmap[s[i]]=1+hashmap.get(s[i],0)   
     
+    #Loop through the second hashmap and check if the element   present in 2nd string is in the hashmap
+    #if its not present simply return false else return true.
+    for j in range(0,len(t)):
+        if t[j] not in hashmap:
+            return False
+        if t[j] in hashmap and not hashmap[t[j]]>0:
+            return False
+        if t[j] in hashmap and hashmap[t[j]]>0:
+            hashmap[t[j]]=1-hashmap.get(t[j],0)
+    return True
+            
+print(isAnagram())
         
