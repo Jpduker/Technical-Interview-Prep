73d070c19ada24e94ab5f595597b8b8dc6acb6f3
Jp <jayaprakashnis619@gmail.com>
2023-01-19 15:05:04 +0530

    strtoInt passed all

--- Coding_Problems/Arrays,Strings,Linkedlists/strToInt.py
+++ Coding_Problems/Arrays,Strings,Linkedlists/strToInt.py
@@ -7,24 +7,32 @@ def myAtoi(s):
     res=0
     negative =1    
     #whitespaces
-    while i<=len(s) and s[i]==" ":
+    while i<len(s) and s[i]==" ":
         i+=1
     
     # + or - Character 
-    while i<=len(s) and s[i]=="-" :
+    if i<len(s) and s[i]=="-" :
         negative =-1
         i+=1
-        
+    elif i<len(s) and s[i]=="+":
+        i+=1
     hashmap= set("0123456789")
     
     #If we encounter numbers (0 to 9)
-    while i<=len(s) and s[i] in hashmap:
-        if res>MAX_INT/10 or (res ==MAX_INT /10 and int(s[i])>7):
-            return MAX_INT if negative==1 else MIN_INT
+    while i<len(s) and s[i] in hashmap:
+        print(s[i])
+        print(MAX_INT//10)
+        if res > (MAX_INT//10) or (res == MAX_INT//10 and int(s[i])> 7):
+            if negative==1:
+                return MAX_INT 
+            else:
+                return MIN_INT
+             
         res = res*10 + int(s[i])
         i+=1
     
     #if the number is negative it will give the correct sign
+ 
     return res*negative
 
-p
+print(myAtoi("2147483648"))
