b0540b7cde3dc96d0b41f15345012ccdd5e1aa5a
Jp <jayaprakashnis619@gmail.com>
2023-01-19 13:44:03 +0530

    added strtoint efficient solution

--- Coding_Problems/Arrays,Strings,Linkedlists/strToInt.py
+++ Coding_Problems/Arrays,Strings,Linkedlists/strToInt.py
@@ -1,12 +1,30 @@
 def myAtoi(s):
-    res=""
-    nums =[0,1,2,3,4,5,6,7,8,9]
-    for i in range(0,len(s)):
-        if int(s[i]) in nums:
-            # if s[0] == "-" or s[0] == "+"" 
-            res+=s[i]
+    
+    MAX_INT = 2**31 -1
+    MIN_INT =-2**31
+    
+    i=0
+    res=0
+    negative =1    
+    #whitespaces
+    while i<=len(s) and s[i]==" ":
+        i+=1
+    
+    # + or - Character 
+    while i<=len(s) and s[i]=="-" :
+        negative =-1
+        i+=1
         
-        
-    res=int(res)
-    return res
-myAtoi("   -42")
+    hashmap= set("0123456789")
+    
+    #If we encounter numbers (0 to 9)
+    while i<=len(s) and s[i] in hashmap:
+        if res>MAX_INT/10 or (res ==MAX_INT /10 and int(s[i])>7):
+            return MAX_INT if negative==1 else MIN_INT
+        res = res*10 + int(s[i])
+        i+=1
+    
+    #if the number is negative it will give the correct sign
+    return res*negative
+
+p
