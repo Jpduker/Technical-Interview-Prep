42f23075f7be03632029e1bf6b296197c6900390
Jpduker <jayaprakashnis619@gmail.com>
2023-02-07 07:30:49 +0530

    added 3Sum two pointer

---
+++ Coding_Problems/Arrays,Strings,Linkedlists/3Sum.py
@@ -0,0 +1,24 @@
+def threeSum( nums):
+        res=[]
+        nums.sort()
+        
+        for i , a in enumerate(nums):
+            if i > 0 and a== nums[i-1]:
+                continue
+            l,r = i+1, len(nums)-1
+            
+            while l<r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum >0:
+                    r -=1
+                elif threeSum<0:
+                    l +=1
+                else:
+                    res.append([a,nums[l],nums[r]])
+                    l+=1
+                    while l<r and nums[l]==nums[r]:
+                        l+=1
+        return res
+nums=[-1,2,-3,2,-2,-4,2]
+
+print(threeSum(nums))
