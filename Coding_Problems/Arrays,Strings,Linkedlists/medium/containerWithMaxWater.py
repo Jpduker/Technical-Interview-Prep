935be45967711b4153ec605e13827022751e9ca3
Jpduker <jayaprakashnis619@gmail.com>
2023-02-08 07:50:26 +0530

    aligned

--- Coding_Problems/Arrays,Strings,Linkedlists/medium/containerWithMaxWater.py
+++ Coding_Problems/Arrays,Strings,Linkedlists/medium/containerWithMaxWater.py
@@ -1,12 +1,12 @@
- def maxArea(self, height: List[int]) -> int:
-        maxArea = 0
-        left , right = 0,len(height)-1
-        
-        while left<right:
-            width = right - left 
-            maxArea = max(maxArea , min(height[left], height[right])*width)
-            if height[left] >height[right]:
-                right -=1
-            else :
-                left +=1
-        return maxArea
+def maxArea(self, height: List[int]) -> int:
+    maxArea = 0
+    left , right = 0,len(height)-1
+    
+    while left<right:
+        width = right - left 
+        maxArea = max(maxArea , min(height[left], height[right])*width)
+        if height[left] >height[right]:
+            right -=1
+        else :
+            left +=1
+    return maxArea
